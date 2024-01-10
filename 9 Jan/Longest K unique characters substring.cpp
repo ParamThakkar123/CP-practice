0319f9a01466850cabf2432bae3c23ab799b9dbc
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-01-10 21:35:29 +0530

    Create Longest K unique characters substring.cpp

---
+++ 9 Jan/Longest K unique characters substring.cpp
@@ -0,0 +1,32 @@
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        unordered_map<char, int> mp;
+        int i = 0;
+        int j = 0;
+        
+        int ans = -1;
+        
+        while(j < s.length()){
+            mp[s[j]]++;
+            if(mp.size() < k){
+                j++;
+            }
+            else if(mp.size() == k){
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+            else if(mp.size() > k){
+                while(mp.size() > k){
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0) mp.erase(s[i]);
+                    i++;
+                }
+                
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
