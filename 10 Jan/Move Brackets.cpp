83dde6059fa2d1708d54569b61acf463b2544d8e
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-01-15 22:01:20 +0530

    Create Move Brackets.cpp

---
+++ 10 Jan/Move Brackets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int ans = 0, bal = 0;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                bal++;
+            }
+            else{
+                bal--;
+                if(bal < 0){
+                    bal = 0;
+                    ans++;
+                }
+            }
+        }
+
+        cout << ans << endl;
+    }
+}
